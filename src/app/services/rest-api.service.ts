d0e14aae8691f12415582f287db6db0db89b39bd
neha-kothari <neha.kothari05@gmail.com>
2021-05-13 23:29:14 +0530

    initialing the service module

--- src/app/services/rest-api.service.ts
+++ src/app/services/rest-api.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient,) { }
+  categorySpecific_response:any;
+  // -----------------------------DASHBOARD - category6-----------------------------------
+  async categorySpecific(){
+    await this.http.get(`https://mission360.ddns.net/api/v1/category/Business`).toPromise()
+      .then((response) => {
+        this.categorySpecific_response = response;
+      });
+    return this.categorySpecific_response;
+  }
 }
